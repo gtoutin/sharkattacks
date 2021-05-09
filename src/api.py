fc242439831dbe0f2f4acda08c0e72b832fc2c3c
Gabrielle Toutin <gctoutin@gmail.com>
2021-05-09 01:40:16 -0500

    added some paths

--- src/api.py
+++ src/api.py
@@ -27,15 +27,16 @@ def info():
   return """
   Routes:
 
-  /					info
-  /loaddata/				load data into Redis DB
-  /records/info/			information about the records
-  /records/<attribute>/<value>/		WIP get all records with an attribute with a certain value
-  /records/contains/<word>/		TODO get all records that have a certain word
-  /records/<record id>/			TODO get a specific record
-  /job/<job id>/			view the information about a submitted job
-  /result/<job id>/			WIP see the result of a submitted job
-  /viz/<attrib>/<startdate>/<enddate>/	TODO vizualize an attribute from start to end date.
+  /						info
+  /loaddata/					load data into Redis database
+  /records/info/				information about the records
+  /records/attribval/<attribute>/<value>/	get all records with an attribute with a certain value
+  /records/contains/<word>/			get all records that have a certain word
+  /records/id/<record id>/			get a specific record
+  /records/delete/<record id>/			TODO delete a specific record
+  /job/<job id>/				view the information about a submitted job
+  /result/<job id>/				see the result of a submitted job
+  /viz/<attrib>/<startyear>/<endyear>/		TODO visualize an attribute over a selection of years. Available attributes are Age, Sex, Fatal.
 
 """
 
@@ -43,7 +44,7 @@ def info():
 @app.route('/records/info/', methods=['GET'])
 def record_info():
   return """
-  ATTRIBUTE	DESCRIPTION
+  ATTRIBUTE		DESCRIPTION
   original order	A number that uniquely identifies a record
   Date			Usually year, sometimes descriptive and inexact, sometimes down to the day
   Year			Numerical year or 0000
@@ -54,7 +55,7 @@ def record_info():
   Activity		Details about what happened
   Name			Name of the person (or gender or who they were)
   Sex			F, M, or ?
-  Age			numerical age or ?
+  Age			Numerical age or ?
   Injury		Details about the injury
   Fatal			Y, N, or ?
   Time			When attack happened or ?
@@ -94,6 +95,26 @@ def getjob(wantedid):
 @app.route('/result/<wantedid>/', methods=['GET'])
 def getresult(wantedid):
   return rd.hget('job.'+wantedid, 'result')
+
+
+# return a specific record
+@app.route('/records/id/<wantedid>/', methods=['GET'])
+def getrecord(wantedid):
+  jobdict = {
+  'type': 'getrecord',
+  'wantedid': str(wantedid) } 
+  jobdict = jobs.add_job(jobdict)
+  return "Submitted job "+jobdict['jid']
+
+
+# return all records containing a specific string
+@app.route('/records/contains/<word>/', methods=['GET'])
+def record_contains(word):
+  jobdict = {
+  'type': 'contains',
+  'word': word }
+  jobdict = jobs.add_job(jobdict)
+  return "Submitted job "+jobdict['jid']
 
 
 if __name__=='__main__':
--- src/worker.py
+++ src/worker.py
@@ -22,8 +22,6 @@ def get_data():  # gets all data in the redis db
 
 
 def attribval(indict):  # get all records with attribute of value
-  print('job',indict['jid'],'has been received',file=sys.stderr)
-  jobs.update_job_status(indict['jid'], 'in progress')
   records = get_data()  # get the db data
   attrib = indict['attrib']
   value = indict['value']
@@ -33,23 +31,46 @@ def attribval(indict):  # get all records with attribute of value
   print(len(output),file=sys.stderr)
   return output
 
-#  rd.hset(jid, 'result', str(output))  # dump the output list to a string and put it in  !!!! FIX THIS ITS ALMOST RIGHT
 
-#  jobs.update_job_status(jid, 'complete')
+def getrecord(indict):  # get the record corresponding to the original order given (original order is the db key)
+  wantedkey = indict['wantedid']
+  try:
+    output = data.hgetall(wantedkey)
+  except:
+    output = []
+    print('no data with that key found', file=sys.stderr)
+  return output
+
+
+def record_contains(indict):  # get the records containing the word specified
+  word = indict['word']  # the word requested
+  records = get_data()
+  
+  output = [ x for x in records if ( word in x['Date'] or word in x['Type'] or word in x['Country'] or word in x['Area'] or word in x['Location'] or word in x['Activity'] 
+    or word in x['Name'] or word in x['Injury'] or word in x['Time'] or word in x['Species'] ) ]
+  return output
 
 
 @q.worker
 def runjobs(jid):  # passes in a job key so worker can get the job off the queue
   print('worker activated', file=sys.stderr)
 
-  indict = rd.hgetall(jobs.generate_job_key(jid))
+  indict = rd.hgetall(jobs.generate_job_key(jid))  # get all relevant job information
 
+  print('job',indict['jid'],'has been received',file=sys.stderr)
+  jobs.update_job_status(indict['jid'], 'in progress')
+
+  # send the job info to the appropriate function
   if indict['type'] == 'attribval':
-    output = attribval(indict)  # do the appropriate job function
+    output = attribval(indict)  # want all with key:value pair
+  if indict['type'] == 'getrecord':
+    output = getrecord(indict)  # want a job with a specific id (original order, which is the key in the data db)
+  if indict['type'] == 'contains':
+    output = record_contains(indict)
 
-  rd.hset(f'job.{jid}', 'result', str(output))
-  jobs.update_job_status(jid, 'complete')
-  print('try to update status to complete',file=sys.stderr)
+  rd.hset(f'job.{jid}', 'result', str(output))  # put the result in the job entry in the db
+  jobs.update_job_status(jid, 'complete')  # now it's done
+  print('updated status to complete',file=sys.stderr)
 
 
 if __name__ == '__main__':
